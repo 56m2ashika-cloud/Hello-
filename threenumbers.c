38d2179b9f91cc33e29c1f75e26c90c0a705fabe
56m2ashika-cloud <56m2ashika@gmail.com>
2025-11-28 20:26:14 +0530

    Add files via upload

---
+++ threenumbers.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the values of a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		printf("a is max");
+	}
+	else
+	{
+		printf("b is max");
+	}
+	if(a>c)
+	{
+		printf("a is max");
+	}
+	else
+	{
+		printf("c is max");
+	}
+	if(b>c)
+	{
+		printf("b is max");
+	}
+	else
+	{
+		printf("c is max");
+	}
+	
+		
+	
+		
+	
+		
+	
+
+	
+
+}
