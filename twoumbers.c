55c6af47fb7ca7bffed1ec41779e64f469e12011
56m2ashika-cloud <56m2ashika@gmail.com>
2025-11-28 20:27:38 +0530

    Add files via upload

---
+++ twoumbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the values of a and b");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("a is max");
+	}
+	else
+	{
+		printf("b is max");
+	}
+
+
+	
+
+	
+	
+}
